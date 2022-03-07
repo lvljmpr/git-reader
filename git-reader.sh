d33f6ed0f7d3d9c4fbfd5b437b9b1a692a0983b4
BAnk <bank8705@gmail.com>
2022-03-07 17:37:28 -0500

    JIRA-027
    bugfixes

--- git-reader.sh
+++ git-reader.sh
@@ -2,10 +2,10 @@
 # Check deps
 jq --help > /dev/null 2>&1
 if [ $? != 0 ]; then
-  echo "Installing 'jq' as it was not found..."
+  printf "Installing 'jq' as it was not found..."
   apt-get update -y; apt-get install -y jq
 fi
-FINALMSG="## CHANGELOG : $ORG/$REPO\n##########\n\n"
+FINALMSG="## CHANGELOG : $ORG/$REPO\n---\n\n"
 function check_err () {
   if [ "$?" != "0" ]; then
     printf "ERR: The start and end SHAs in inputs.json do not appear to be valid."
@@ -13,23 +13,17 @@ function check_err () {
   fi
 }
 function do_work () {
-  curl -H "Authorization: token $GHPAT" "https://api.github.com/repos/$ORG/$REPO/commits/$END" > /dev/null 2>&1
   HEAD=$(curl -H "Authorization: token $GHPAT" "https://api.github.com/repos/$ORG/$REPO/commits/$END")
-  check_err
   MESSAGE=$(echo $HEAD| jq -r '.commit.message')
   PARENT=$(echo $HEAD| jq -r '.parents[].sha')
   END="$PARENT"
-  FINALMSG="$FINALMSG\n$MESSAGE"
+  FINALMSG="$FINALMSG\n---\n$MESSAGE\n"
 }
+curl -H "Authorization: token $GHPAT" "https://api.github.com/repos/$ORG/$REPO/commits/$END" > /dev/null 2>&1
+check_err
 while [ "$PARENT" != "$BASE" ]; do
   do_work
 done
-if [ "$PARENT" = "$BASE" ]; then
-  do_work
-  do_work
-else
-  printf "ERR: Could not produce commit list with inputs.json provided."
-  exit 1
-fi
+do_work
 printf "$FINALMSG"
 exit 0
