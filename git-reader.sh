be92e513196fff9a9df862a06f37c3915fbc6cbe
BAnk <bank8705@gmail.com>
2022-03-07 17:44:41 -0500

    JIRA-030
    pretty it up a bit

--- git-reader.sh
+++ git-reader.sh
@@ -5,7 +5,7 @@ if [ $? != 0 ]; then
   printf "Installing 'jq' as it was not found..."
   apt-get update -y; apt-get install -y jq
 fi
-FINALMSG="## CHANGELOG : $ORG/$REPO\n"
+FINALMSG="## CHANGELOG : $ORG/$REPO\n---\n"
 function check_err () {
   if [ "$?" != "0" ]; then
     printf "ERR: The start and end SHAs in inputs.json do not appear to be valid."
@@ -13,11 +13,11 @@ function check_err () {
   fi
 }
 function do_work () {
-  HEAD=$(curl -H "Authorization: token $GHPAT" "https://api.github.com/repos/$ORG/$REPO/commits/$END")
+  HEAD=$(curl -s -H "Authorization: token $GHPAT" "https://api.github.com/repos/$ORG/$REPO/commits/$END")
   MESSAGE=$(echo $HEAD| jq -r '.commit.message')
   PARENT=$(echo $HEAD| jq -r '.parents[].sha')
   END=$PARENT
-  FINALMSG="$FINALMSG\n---\n$MESSAGE\n"
+  FINALMSG="$FINALMSG\n$MESSAGE\n"
 }
 curl -H "Authorization: token $GHPAT" "https://api.github.com/repos/$ORG/$REPO/commits/$END" > /dev/null 2>&1
 check_err
